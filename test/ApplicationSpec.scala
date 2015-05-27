794e4522fb1c6b52708c2710c280a55579e252aa
Tony McCrae <tmccrae@equalexperts.com>
2015-05-27 14:37:55 +0100

    Do you want tests with that?

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,15 +1,13 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
+import play.api.mvc
 
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
+import scala.concurrent.Future
+
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
@@ -26,5 +24,16 @@ class ApplicationSpec extends Specification {
       contentType(home) must beSome.which(_ == "text/html")
       contentAsString(home) must contain ("Your new application is ready.")
     }
+
+    "show widgets details on the meh pages" in new WithApplication {
+      val request = FakeRequest(GET, "/meh")
+      val routedRequest: Option[Future[mvc.Result]] = route(request)
+      val eventualResult: Future[mvc.Result] = routedRequest.get
+
+      status(eventualResult) must equalTo(OK)
+      contentAsString(eventualResult) must contain ("colour: Red");
+    }
+
   }
+
 }
