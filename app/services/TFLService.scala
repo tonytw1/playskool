4724c1b7ae49acb5b91715eee68184f405d8de1b
Tony McCrae <tmccrae@equalexperts.com>
2015-05-28 16:10:13 +0100

    Clean up and excessive minification.

--- app/services/TFLService.scala
+++ app/services/TFLService.scala
@@ -1,29 +1,24 @@
 package services
 
-import model.{BikePointAdditionalProperty, BikePoint}
+import model.{BikePoint, BikePointAdditionalProperty}
 import play.api.Play.current
 import play.api.libs.json._
-import play.api.libs.ws.{WS, WSRequestHolder, WSResponse}
+import play.api.libs.ws.WS
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class TFLService {
 
+  implicit val readsBikePointAdditionalProperty: Reads[BikePointAdditionalProperty] = Json.reads[BikePointAdditionalProperty]
+  implicit val readsBikePoint: Reads[BikePoint] = Json.reads[BikePoint]
+
   def fetchData(): Future[BikePoint] = {
-    val holder: WSRequestHolder = WS.url("http://api.prod5.live.tfl.gov.uk/Place/BikePoints_195")
-    val eventualResponse: Future[WSResponse] = holder.get
-
-    val result: Future[BikePoint] = eventualResponse.map {
+    WS.url("http://api.prod5.live.tfl.gov.uk/Place/BikePoints_195").get.map {
       response => {
-        implicit val readsBikePointAdditionalProperty: Reads[BikePointAdditionalProperty] = Json.reads[BikePointAdditionalProperty]
-        implicit val readsBikePoint: Reads[BikePoint] = Json.reads[BikePoint]
-
         Json.parse(response.body).as[BikePoint]
       }
-
     }
-    result
   }
 
 }
