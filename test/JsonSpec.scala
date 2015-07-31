dcdfd07ff2ba19dc5b624be5012568f0278c285e
Tony McCrae <tmccrae@equalexperts.com>
2015-07-31 18:12:32 +0100

    Use formatter for vanilla JSON.

--- test/JsonSpec.scala
+++ test/JsonSpec.scala
@@ -8,18 +8,13 @@ class JsonSpec  extends Specification {
   "Play JSON library" should {
 
     "Round trip simple domain objects" in new WithApplication {
-      implicit val reads: Reads[BikePoint] = Json.reads[BikePoint]
-      implicit val writes: Writes[BikePoint] = Json.writes[BikePoint]
-
-      implicit val bikePointAdditionalPropertyReads = Json.reads[BikePointAdditionalProperty]
-      implicit val bikePointAdditionalPropertyWrites = Json.writes[BikePointAdditionalProperty]
-
+      implicit val reads: Format[BikePoint] = Json.format[BikePoint]
+      implicit val bikePointAdditionalPropertyReads: Format[BikePointAdditionalProperty]= Json.format[BikePointAdditionalProperty]
       private val bikePoint: BikePoint = new BikePoint("Somewhere", Seq.empty[BikePointAdditionalProperty])
 
       private val json: String = Json.toJson(bikePoint).toString()
-
       private val roundTripped: BikePoint = Json.parse(json).as[BikePoint]
-
+      
       roundTripped.commonName must equalTo("Somewhere")
     }
 
