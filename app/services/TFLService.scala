8b246ec3b0c2f29bd897dcd75ca76f95721c2f80
Tony McCrae <tmccrae@equalexperts.com>
2015-07-31 18:40:56 +0100

    Caching. So much wrong with this - so few characters to list them all

--- app/services/TFLService.scala
+++ app/services/TFLService.scala
@@ -4,6 +4,7 @@ import model.{BikePoint, BikePointAdditionalProperty}
 import play.api.Play.current
 import play.api.libs.json._
 import play.api.libs.ws.WS
+import play.api.cache.Cache
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -14,11 +15,26 @@ class TFLService {
   implicit val readsBikePoint: Reads[BikePoint] = Json.reads[BikePoint]
 
   def fetchData(id: Int): Future[BikePoint] = {
-    WS.url("https://api.tfl.gov.uk/Place/BikePoints_" + id).get.map {
-      response => {
-        Json.parse(response.body).as[BikePoint]
+    val cacheKey: String = "docking-station-" + id
+
+    var data = Cache.getAs[BikePoint](cacheKey)
+    if (data.isEmpty) {
+      println("Cache miss")
+      WS.url("https://api.tfl.gov.uk/Place/BikePoints_" + id).get.map {
+        response => {
+          Json.parse(response.body).as[BikePoint]
+        }
+      }.map {
+        ds => {
+          Cache.set(cacheKey, ds)
+          data = Some(ds)
+          println("Cached: " + data)
+        }
       }
+    } else {
+      println("Cache hit")
     }
+    Future.successful(data.get)
   }
 
 }
