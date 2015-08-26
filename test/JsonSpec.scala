376bc470b4169d68817a5d63cbe6a923fd196796
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-08-26 19:32:35 +0100

    Marking TODO.

--- test/JsonSpec.scala
+++ test/JsonSpec.scala
@@ -8,8 +8,11 @@ class JsonSpec extends Specification {
   "Play JSON library" should {
 
     "Round trip bike point domain object" in new WithApplication {
+
+      // TODO can the formats be put on the domain object to prevent duplication?
       implicit val bikePointAdditionalPropertyReads: Format[BikePointAdditionalProperty]= Json.format[BikePointAdditionalProperty]
       implicit val reads: Format[BikePoint] = Json.format[BikePoint]
+
       private val bikePoint: BikePoint = new BikePoint("BikePoint_999", "Somewhere", Seq.empty[BikePointAdditionalProperty])
 
       private val json: String = Json.toJson(bikePoint).toString()
