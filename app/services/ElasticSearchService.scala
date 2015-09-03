25f703fc18e1c9857c3c02e3e78a671d05038202
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-09-03 09:18:59 +0100

    Drop in index create example from elastic4s docs.

--- app/services/ElasticSearchService.scala
+++ app/services/ElasticSearchService.scala
@@ -1,9 +1,12 @@
 package services
 
-import com.sksamuel.elastic4s.ElasticClient
+import com.sksamuel.elastic4s.{KeywordAnalyzer, ElasticClient, SimpleAnalyzer}
 import com.sksamuel.elastic4s.ElasticDsl._
+import com.sksamuel.elastic4s.mappings.FieldType.GeoPointType
 import org.elasticsearch.action.search.SearchResponse
 import play.api.Logger
+import com.sksamuel.elastic4s.mappings.FieldType._
+import com.sksamuel.elastic4s.ElasticDsl._
 
 import scala.concurrent.ExecutionContext.Implicits.{global => ec}
 import scala.concurrent.Future
@@ -25,8 +28,18 @@ trait ElasticSearchService {
 
 
   def createIndex(): Unit = {
-    //var mappings: MappingDefinition = new MappingDefinition() // TODO
-    //client.execute {create index "test" mappings {mappings}}
+    client.execute {create index "test" mappings (
+
+      "city" as (
+        "year_founded" typed IntegerType,
+        "location" typed GeoPointType,
+        "demonym" typed StringType nullValue "citizen" analyzer KeywordAnalyzer
+        ),
+      "country" as (
+        "name" typed StringType analyzer SimpleAnalyzer
+        ) dateDetection true dynamicDateFormats("dd/MM/yyyy", "dd-MM-yyyy")
+
+      )}.await
   }
 
   def deleteIndex(): Unit = {
