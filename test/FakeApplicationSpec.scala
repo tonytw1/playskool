19b42161c7c0ef6076ed6f6bc059c70310d4b997
Tony McCrae <tmccrae@equalexperts.com>
2015-09-10 11:03:01 +0100

    Spec runner only

--- test/FakeApplicationSpec.scala
+++ test/FakeApplicationSpec.scala
@@ -3,7 +3,6 @@ import org.specs2.mutable._
 import org.specs2.runner._
 import play.api.test._
 
-@RunWith(classOf[JUnitRunner])
 class FakeApplicationSpec extends Specification {
 
   val fa = FakeApplication(additionalConfiguration = Map(
