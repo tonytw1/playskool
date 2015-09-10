789b04fd2bda5834cdab31b4b6eeef5a740e5b8b
Tony McCrae <tmccrae@equalexperts.com>
2015-09-10 11:00:14 +0100

    Confirm FakeApplication log level controls.

---
+++ test/FakeApplicationSpec.scala
@@ -0,0 +1,22 @@
+import org.junit.runner._
+import org.specs2.mutable._
+import org.specs2.runner._
+import play.api.test._
+
+@RunWith(classOf[JUnitRunner])
+class FakeApplicationSpec extends Specification {
+
+  val fa = FakeApplication(additionalConfiguration = Map(
+    "logger.root" -> "TRACE",
+    "logger.play" -> "TRACE",
+    "logger.application" -> "TRACE"
+  ))
+
+  "Application" should {
+
+    "work from within a fake application" in new WithApplication(fa) {
+      println("Done")
+    }
+    
+  }
+}
