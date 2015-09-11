0538ca7e075bcf219a6b81dd140fa14a665f8633
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-09-11 22:36:02 +0100

    ... then you might be able to this.

--- app/model/FeedItem.scala
+++ app/model/FeedItem.scala
@@ -9,8 +9,15 @@ trait Newsworthy {
   def body: Option[String]
 }
 
+
+trait Tagged {
+  def tags: Seq[Tag]
+}
+
 case class FeedItem(title: String, url: String, imageUrl: Option[String], body: Option[String]) extends Newsworthy
 
 object FeedItem {
   implicit val formats: Format[FeedItem] = Json.format[FeedItem]
 }
+
+case class Newsitem(title: String, url: String, imageUrl: Option[String], body: Option[String], tags: Seq[Tag]) extends Newsworthy with Tagged
