cc48ac60912dcd31c4ead527127867dc855213ce
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-09-12 13:11:22 +0100

    Refactor - split out news items service.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,28 +1,18 @@
 package controllers
 
-import model.{Newsitem, FeedItem}
 import play.api.mvc._
-import services.tagging.AutoTagger
-import services.{WhakaokoService, TFLService}
+import services.newsitems.NewsitemService
+
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 object Application extends Controller {
 
-  val whakaokoService: WhakaokoService = WhakaokoService
-  val autoTagger: AutoTagger = AutoTagger
+  val newsItemService: NewsitemService = NewsitemService
 
   def index = Action.async {
-    val feedItemsFuture: Future[Seq[FeedItem]] = whakaokoService.fetchFeed()
-
-    feedItemsFuture.map(feedItems => {
-
-      val newsitems: Seq[Newsitem] = feedItems.map(i => {
-        Newsitem(i.title, i.url, i.imageUrl, i.body, autoTagger.inferTagsFor(i))
-      })
-
-      Ok(views.html.homepage(newsitems))
-    })
+    newsItemService.latest.map(ns =>
+      Ok(views.html.homepage(ns)
+    ))
   }
 
 }
---
+++ app/services/newsitems/NewsitemsService.scala
@@ -0,0 +1,25 @@
+package services.newsitems
+
+import model.Newsitem
+import services.WhakaokoService
+import services.tagging.AutoTagger
+
+import scala.concurrent.ExecutionContext.Implicits.{global => ec}
+import scala.concurrent.Future
+
+trait NewsitemService {
+
+  val whakaokoService: WhakaokoService = WhakaokoService
+  val autoTagger: AutoTagger = AutoTagger
+
+  def latest(): Future[Seq[Newsitem]] = {
+    whakaokoService.fetchFeed().map(feedItems => {
+      feedItems.map(i => {
+        Newsitem(i.title, i.url, i.imageUrl, i.body, autoTagger.inferTagsFor(i))
+      })
+    })
+  }
+
+}
+
+object NewsitemService extends NewsitemService
