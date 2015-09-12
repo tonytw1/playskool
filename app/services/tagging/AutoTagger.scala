22d212c53b2860a7f32399cb9240d6bafa9ba062
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-09-12 13:00:23 +0100

    The auto tagger should work on all newsworthy things.

--- app/services/tagging/AutoTagger.scala
+++ app/services/tagging/AutoTagger.scala
@@ -1,19 +1,14 @@
 package services.tagging
 
-import model.{Newsitem, Tag, FeedItem}
-import play.api.Logger
-import play.api.Play.current
-import play.api.libs.json._
-import play.api.libs.ws.WS
+import model.{Newsworthy, Tag}
 
 import scala.concurrent.ExecutionContext.Implicits.{global => ec}
-import scala.concurrent.Future
 
 trait AutoTagger {
 
   val availableTags = Seq(Tag("Rugby"), Tag("Victoria University"), Tag("Upper Hutt"))
 
-  def inferTagsFor(item: FeedItem): Seq[Tag] = {
+  def inferTagsFor(item: Newsworthy): Seq[Tag] = {
     availableTags.filter(t => {
       item.title.toLowerCase.contains(t.name.toLowerCase)
     })
