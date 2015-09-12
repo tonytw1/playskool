89b9368afd8a1708cebf2b893c1ddd6caf62d680
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-09-12 12:42:28 +0100

    Scala specs practise.

---
+++ test/model/AutoTaggerSpec.scala
@@ -0,0 +1,29 @@
+package model
+
+import org.specs2.mutable._
+import services.tagging.AutoTagger
+
+class AutoTaggerSpec extends Specification {
+
+  "Auto tagger" should {
+
+    val autoTagger = AutoTagger
+    "match tag names in feeditem titles" in {
+
+      val tags = autoTagger.inferTagsFor(FeedItem("Something mentioning Rugby", "http://localhost/123", None, None))
+
+      tags must have length(1)
+      tags must contain(Tag("Rugby"))
+    }
+
+    "be case insenitice when matching tag names in titles" in {
+
+      val tags = autoTagger.inferTagsFor(FeedItem("Something mentioning rugby", "http://localhost/123", None, None))
+
+      tags must have length(1)
+      tags must contain(Tag("Rugby"))
+    }
+
+  }
+
+}
