f4eb94de8e45baa15ad557125cf1a558511cef8b
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-09-28 08:11:25 +0100

    Tags returns from mongo.

--- app/services/tagging/Tags.scala
+++ app/services/tagging/Tags.scala
@@ -21,8 +21,12 @@ trait Tags {
 
   def all(): Future[Seq[Tag]] = {
     val tags: Future[Seq[Tag]] = tagService.fetchTags()
+
     tags.map(ts => ts.foreach(t => mongoService.writeTag(t)))
-    tags
+
+    // To the write future hasn't completed before the next read
+
+    mongoService.readTags()
   }
 
 }
