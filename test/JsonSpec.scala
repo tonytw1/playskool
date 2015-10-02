eda668cdfc661db633af89e574618336474c989b
Tony McCrae <tmccrae@equalexperts.com>
2015-10-02 17:02:34 +0100

    ISO date time formats

--- test/JsonSpec.scala
+++ test/JsonSpec.scala
@@ -1,5 +1,6 @@
 import model.{FeedItem, BikePointAdditionalProperty, BikePoint}
 import org.joda.time.DateTime
+import org.joda.time.format.{ISODateTimeFormat, DateTimeFormatter}
 import org.specs2.mutable.Specification
 import play.api.libs.json.Json.JsValueWrapper
 import play.api.libs.json._
@@ -37,6 +38,47 @@ class JsonSpec extends Specification {
       private val roundTripped = Json.parse(json).as[Seq[FeedItem]]
       println(roundTripped)
     }
+
+    "Be able to round trip date times to ISO date format" in new WithApplication {
+
+
+      implicit val df: Format[DateTime] = new Format[DateTime] {
+
+        private val iso : DateTimeFormatter = ISODateTimeFormat.dateTimeNoMillis();
+
+         override def writes(o: DateTime): JsValue = {
+           JsString(iso.print(o))
+        }
+
+        override def reads(json: JsValue): JsResult[DateTime] = {
+          json match {
+            case JsString(s) => JsSuccess(DateTime.now)
+            case _ => throw new RuntimeException()
+          }
+        }
+      }
+
+      case class DatedWidget(id: String, date: DateTime)
+
+      val widget = DatedWidget("123", DateTime.now)
+
+      implicit val formats: Format[DatedWidget] = Json.format[DatedWidget]
+
+      val json = Json.toJson(widget)
+      val jsonString = json.toString()
+
+      println("As JSON string: " + jsonString)
+
+      private val roundTrippedJson = Json.parse(jsonString)
+
+      val roundTrippedWidget = roundTrippedJson.as[DatedWidget]
+
+      println("Round tripped: " + roundTrippedWidget)
+      println("Round tripped date: " + roundTrippedWidget.date)
+    }
+
   }
+
+
 
 }
