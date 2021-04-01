705b39dd04ec2a536d4abc44dcbb8b860f3f29a6
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2021-04-01 09:26:56 +0100

    Show the workings around the Future API call.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,23 +1,33 @@
 package controllers
 
-import play.api.Configuration
+import model.BikePoint
 import play.api.mvc._
+import play.api.{Configuration, Logger}
 import services.TFLService
 import views.WithHeader
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class Application @Inject()(config: Configuration, TFLService: TFLService) extends Controller with WithHeader {
 
-  val tflService: TFLService = TFLService
-  val id = config.get[Int]("dockingstation")
+  private val tflService = TFLService
 
-  def dockingStation() = Action.async {
-    for {
-      ds <- tflService.fetchBikePoint(id)
-    } yield {
-      Ok(views.html.docking_station(ds))
+  private val defaultDockingStation = config.get[Int]("dockingstation")
+
+  def homepage() = Action.async {
+
+    // Represents a value with will resolve in the Future; non blocking etc
+    // The actual work may happen in another thread.
+
+    Logger.info("Requesting bike point")
+    val eventualBikePoint: Future[BikePoint] = tflService.fetchBikePoint(defaultDockingStation)
+    Logger.info("Dropped though to this line immediately")
+
+    eventualBikePoint.map { bikePoint =>
+      Logger.info("Bike point is now available")
+      Ok(views.html.docking_station(bikePoint))
     }
   }
 
